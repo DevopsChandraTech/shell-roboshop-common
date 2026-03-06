75f378ddde82943e445e9a053ea81ea7278160df
DevOpsChandraTech <devopschandra42@gmail.com>
2026-03-06 10:28:22 +0530

    shell-roboshop-common

---
+++ common.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#colours for script
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+#log folder creation 
+LOG_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+START_TIME=$(date +%s)
+mkdir -p /var/log/shell-roboshop
+
+# checks user with root priviliges or not
+USER_ID=$(id -u)
+CHECK_ROOT(){
+    if [ $USER_ID -ne 0 ]; then
+        echo "Error:: Run Command With Root User Privilizes."
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+
+PRINT_TOTAL_TIME(){
+    END_TIME=$(date +%s)
+    TOTAL_TIME=$(($END_TIME - $START_TIME))
+    echo -e "the script executed in: $Y $TOTAL_TIME $N Seconds."
+}
---
+++ mongodb.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+source ./common.sh #calling common code using this source is source of the file and . -> current directory
+
+CHECK_ROOT #checks root user privilizes or not in common script
+
+PRINT_TOTAL_TIME # prints the total time of the script
+
+
+#copy mongodb repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Adding Mongo Repo" 
+
+#install mongodb
+dnf install mongodb-org -y &>> $LOG_FILE
+VALIDATE $? "Installing Mongo Repo"
+
+# enable mongodb
+systemctl enable mongod | tee -a $LOG_FILE
+VALIDATE $? "Enable Mongo Repo"
+
+#start mongodb
+systemctl start mongod | tee -a $LOG_FILE
+VALIDATE $? "Start Mongo Repo"
+
+# using sed update ip address for remote connection
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing Remote Connection"
+
+#restart mongodb service
+systemctl restart mongod
+VALIDATE $? "Restart Mongodb Service"
