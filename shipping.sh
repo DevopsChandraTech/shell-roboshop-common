4be8a8c603d2bbdd56775dd111fb7217f4126ec2
DevOpsChandraTech <devopschandra42@gmail.com>
2026-03-09 11:33:48 +0530

    shell-roboshop-common

--- shipping.sh
+++ shipping.sh
@@ -7,6 +7,7 @@ JAVA_SETUP
 SYSTEMD_SETUP
 RESTART_SERVICE
 
+dnf install mysql -y &>> $LOG_FILE
 mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities' &>>$LOG_FILE
 if [ $? -ne 0 ]; then
     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
