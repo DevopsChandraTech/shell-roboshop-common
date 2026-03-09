ec356307845a4cf63cf83e7173a8bba076d484ce
DevOpsChandraTech <devopschandra42@gmail.com>
2026-03-09 10:52:19 +0530

    shell-roboshop-common

--- mysql.sh
+++ mysql.sh
@@ -4,13 +4,13 @@ source ./common.sh #calling common code using this source is source of the file 
 
 CHECK_ROOT #checks root user privilizes or not in common script
 
-dnf install mysql-server -y
+dnf install mysql-server -y &>>LOG_FILE
 VALIDATE $? "Installing MySql"
-systemctl enable mysqld
+systemctl enable mysqld &>>LOG_FILE
 VALIDATE $? "Enable MySql"
-systemctl start mysqld 
+systemctl start mysqld &>>LOG_FILE
 VALIDATE $? "Start MySql" 
-mysql_secure_installation --set-root-pass RoboShop@1
+mysql_secure_installation --set-root-pass RoboShop@1 &>>LOG_FILE
 VALIDATE $? "Set Password"
 
 PRINT_TOTAL_TIME
