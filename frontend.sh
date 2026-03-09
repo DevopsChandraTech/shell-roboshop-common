488d7d5f2946d30474938dfb340c14b3244f9166
DevOpsChandraTech <devopschandra42@gmail.com>
2026-03-09 13:56:29 +0530

    shell-roboshop-common

--- frontend.sh
+++ frontend.sh
@@ -3,6 +3,10 @@ source ./common.sh
 CHECK_ROOT
 NGINX_SETUP
 
+systemctl enable nginx  &>>$LOG_FILE
+VALIDATE $? "Enable Nginx"
+systemctl start nginx 
+VALIDATE $? "Start Nginx"
 rm -rf /usr/share/nginx/html/* &>> $LOG_FILE
 VALIDATE $? "Remove Default Html"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>> $LOG_FILE
