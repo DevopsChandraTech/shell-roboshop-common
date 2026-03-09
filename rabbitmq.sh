0d0fb129cd6305ff433385870ff9f07c0def28d5
DevOpsChandraTech <devopschandra42@gmail.com>
2026-03-09 11:58:07 +0530

    shell-roboshop-common

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,2 +1,18 @@
+#!/bin/bash
+source ./common.sh
 
+CHECK_ROOT
+
+cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
 dnf install rabbitmq-server -y
+VALIDATE $? "Installing Rabbitmq Server"
+systemctl enable rabbitmq-server
+VALIDATE $? "Enable rabbitmq server"
+systemctl start rabbitmq-server
+VALIDATE $? "Start Rabbitmq Server"
+rabbitmqctl add_user roboshop roboshop123
+VALIDATE $? "Adding roboshop User"
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+VALIDATE $? "Setting Permissions"
+
+PRINT_TOTAL_TIME
